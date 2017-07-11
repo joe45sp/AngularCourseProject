be2f38d15bae832c623be8b35f2faa02be72f79d
Joseph Van Wagner <jvanwagner@anexinet.com>
2017-07-11 15:16:38 -0400

    Added required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shoppinglist/shopping-list/shopping-list.component';
+import { EditFormComponent } from './shoppinglist/edit-form/edit-form.component';
+import { RecipeListComponent } from './recipebook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipebook/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipebook/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    EditFormComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
