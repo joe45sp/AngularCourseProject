cdf898893899f02a5f42c042f2b146e96f1f616a
Joseph Van Wagner <jvanwagner@anexinet.com>
2017-07-11 16:10:51 -0400

    Added model and changed recipe list component to read from array of the model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://s17-us2.ixquick.com/cgi-bin/serveimage?url=http:%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F250x250%2F1167319.jpg&sp=4db49da3ae8a8aaf76bc7bb87d2d9bfd')
+  ];
 
   constructor() { }
 
